4e9f1ba161f4f6688090ecc8a5291bada90306e3
ajay9686 <ajaykumbar05@gmail.com>
2025-06-11 16:56:01 +0530

    PositiveCheck.java

--- PositiveCheck.java
+++ PositiveCheck.java
@@ -1,9 +1,1 @@
-public class PositiveCheck {
-    public static void main(String[] args) {
-        int num = 10;
 
-        if (num > 0) {
-            System.out.println("Positive");
-        }
-    }
-}
